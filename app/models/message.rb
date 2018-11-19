8120e8043e18fe502839d6907eafb6eb7805ce18
AnthonyFrancis <tonytrack@gmail.com>
2018-11-19 16:37:09 +0000

    associated messages to user table

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
 	validates :to, presence: true
   	validates :from, presence: true
   	validates :text, presence: true
+
+  	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_160133) do
+ActiveRecord::Schema.define(version: 2018_11_19_163540) do
 
   create_table "calls", force: :cascade do |t|
     t.string "to"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2018_11_19_160133) do
     t.datetime "updated_at", null: false
     t.string "to"
     t.string "from"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "sms", force: :cascade do |t|
