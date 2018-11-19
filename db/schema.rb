60b56441886b1dcfd85a42fcf2e6cb705c11c539
AnthonyFrancis <tonytrack@gmail.com>
2018-11-19 16:58:35 +0000

    phone number updates in the user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_163540) do
+ActiveRecord::Schema.define(version: 2018_11_19_164401) do
 
   create_table "calls", force: :cascade do |t|
     t.string "to"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_11_19_163540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
